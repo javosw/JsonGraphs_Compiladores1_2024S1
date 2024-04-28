efb3f6153fd3c58f633b093304e36c6fd9bbda96
JavierOswaldo <javoswsq@gmail.com>
2024-04-27 23:48:43 -0600

    creacion de parser para dashboard

---
+++ codigo/lenguajes/src/main/java/josq/lenguajes/Lenguajes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package josq.lenguajes;
+
+/**
+ *
+ * @author JavierOswaldo
+ */
+public class Lenguajes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
